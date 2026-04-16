479009cb6b926b787c0cf9d6917655c1bd4ffea0
KyuWa <220395610@aston.ac.uk>
2026-04-16 23:37:03 +0100

    - Save max BE in config

--- src/main/kotlin/org/kyowa/familyaddons/FamilyAddons.kt
+++ src/main/kotlin/org/kyowa/familyaddons/FamilyAddons.kt
@@ -16,7 +16,7 @@ val COLOR_CODE_REGEX = Regex("§.")
 object FamilyAddons : ClientModInitializer {
 
     val LOGGER = LoggerFactory.getLogger("FamilyAddons")
-    const val VERSION = "1.0.1"
+    const val VERSION = "1.0.2"
     const val MC_VERSION = "1.21.10"
 
     private var hudEditorMouseWasDown = false
